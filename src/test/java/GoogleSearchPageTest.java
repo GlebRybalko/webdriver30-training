35c42ef9e82236b80d4bdca34d8ca46fbf1c9294
glib.rybalko <glebrybalko@gmail.com>
2016-12-14 15:33:23 +0200

    Google search test added

--- src/test/java/GoogleSearchPageTest.java
+++ src/test/java/GoogleSearchPageTest.java
@@ -1,5 +1,16 @@
 /**
- * Created by Gleb on 14.12.2016.
+ * Created by glib.rybalko on 14.12.2016.
  */
-public class GoogleSearchPageTest {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class GoogleSearchPageTest extends BaseTest {
+    @Test
+    public void googleSearchByNameTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Google Search"));
+    }
 }
