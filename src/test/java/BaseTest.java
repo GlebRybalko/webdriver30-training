55801cc33a34624980683d22b9f4b571ec4d23f1
GlebRybalko <glebrybalko@gmail.com>
2016-12-14 22:10:43 +0200

    Update Base Test to work with ChromeDriver Remove complex Waits logic for simplicity sake

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,30 +3,18 @@
  */
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
 
-        public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
         public WebDriver driver;
         public WebDriverWait wait;
 
         @Before
         public void start() {
-            if (tlDriver.get() != null) {
-                driver = tlDriver.get();
-                wait = new WebDriverWait(driver, 10);
-                return;
-            }
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setCapability(FirefoxDriver.MARIONETTE, false);
-            driver = new FirefoxDriver(caps);
-            tlDriver.set(driver);
-            System.out.println(((HasCapabilities) driver).getCapabilities());
+            driver = new ChromeDriver();
             wait = new WebDriverWait(driver, 10);
 
         }
