a300b11d7072e9325c5becc0e8206820d19923f1
GlebRybalko <glebrybalko@gmail.com>
2016-12-18 17:07:48 +0200

    Add infrastructure to run tests in parallel threads.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,20 +3,39 @@
  */
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 public class BaseTest {
 
+}
+        public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
         public WebDriver driver;
         public WebDriverWait wait;
 
         @Before
         public void start() {
-            driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, 10);
+                if (tlDriver.get() != null) {
+                    driver = tlDriver.get();
+                    wait = new WebDriverWait(driver, 10);
+                    return;
+                }
 
+                driver = new ChromeDriver();
+                tlDriver.set(driver);
+                wait = new WebDriverWait(driver, 10);
+
+                Runtime.getRuntime().addShutdownHook(
+                        new Thread(() -> { driver.quit(); driver = null; }));
         }
 
         @After
